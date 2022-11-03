ef2ad21563856773ee1251b2eb3b044ef605914b
bello <bellosoliu12@gmail.com>
2022-11-03 21:31:31 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductaddComponent } from './productadd/productadd.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainpageComponent },
+  { path: 'mainpage', component: MainpageComponent },
+  { path: 'productlist', component: ProductlistComponent },
+  { path: 'productadd', component: ProductaddComponent },
+  { path: 'productadd/:id', component: ProductaddComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductaddComponent,
+    ProductlistComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
+    AlertModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
